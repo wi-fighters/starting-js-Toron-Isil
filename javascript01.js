017ab80836aa51783b4d15513e4c8f3a86c25518
Toron-Isil <t.isil@arcor.de>
2019-08-20 15:34:55 +0200

    JavaScript-Lessons Math 1-3

---
+++ javascript01.js
@@ -0,0 +1,21 @@
+// #01
+
+const numbers = (2, 35, -27, 73, -8, -38, 105, -99);
+
+console.log(Math.min(numbers));
+
+console.log(\n);
+// #02
+
+const numbers = (2, 35, -27, 73, -8, -38, 105, -99);
+
+console.log(Math.max(numbers));
+
+console.log(\n);
+// #03
+
+const numbers = (3321.32321, 326.76, 76788.7, -9.78, 43.342);
+
+console.log(Math.round(numbers));
+
+console.log(\n);
